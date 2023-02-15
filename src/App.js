e587eeb345d7cc68d2158a0169bebebd3f26fa4a
ZedPriest <Charleschukwuemeka47@gmail.com>
2023-02-16 00:09:56 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { useEffect, useState } from "react";
 import { Contract, providers } from "ethers";
 function App() {
-    const [isWalletInstalled, setIsWalletInstalled] = useState(false);
-    // state for keeping track of current connected account.
-    const [account, setAccount] = useState(null);
-    
-    useEffect(() => {
-        if (window.ethereum) {
-            setIsWalletInstalled(true);
-        }
-    }, []);
-    
-    async function connectWallet() {
-        window.ethereum
-            .request({
-                method: "eth_requestAccounts",
-            })
-            .then((accounts) => {
-                setAccount(accounts[0]);
-            })
-            .catch((error) => {
-                alert("Something went wrong");
-            });
-        }
-        
-        if (account === null) {
-            return (
-                <div className="App">
-                    { 
-                        isWalletInstalled ? (
-                           <div class = "middle"> <button class="btn" onClick={connectWallet}>Connect Wallet</button></div> 
-                        ) : (
-                            <div class = "middle"><h3> You don't have a Metamask Wallet, please kindly Install Metamask wallet</h3></div>
-                            
-                        )
-                    }
-                </div>
-             );
-         }
-             return (
-                 <div className="App"> 
-                     <p>Connected as: {account}</p>
-                 </div>
-             ); 
-         }
+  const [isWalletInstalled, setIsWalletInstalled] = useState(false);
+  // state for keeping track of current connected account.
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      setIsWalletInstalled(true);
+    }
+  }, []);
+
+  async function connectWallet() {
+    window.ethereum
+      .request({
+        method: "eth_requestAccounts",
+      })
+      .then((accounts) => {
+        setAccount(accounts[0]);
+      })
+      .catch((error) => {
+        alert("Something went wrong");
+      });
+  }
+
+  if (account === null) {
+    return (
+      <div className="App">
+        {isWalletInstalled ? (
+          <div class="middle">
+            {" "}
+            <button class="btn" onClick={connectWallet}>
+              Connect Wallet
+            </button>
+          </div>
+        ) : (
+          <div class="middle">
+            <h3>
+              {" "}
+              You don't have a Metamask Wallet, please kindly Install Metamask
+              wallet
+            </h3>
+          </div>
+        )}
+      </div>
+    );
+  }
+  return (
+    <div className="App">
+      <p>Connected as: {account}</p>
+    </div>
+  );
+}
 export default App;
